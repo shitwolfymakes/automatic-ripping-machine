fd814185f97c87115d6bb640852ac96f81b5f4bf
wolfy <wolfy@shitwolfymakes.com>
2022-10-17 16:34:48 -0600

    Updated pseudocode

--- arm/ripper/ripper_dvd.py
+++ arm/ripper/ripper_dvd.py
@@ -13,17 +13,25 @@ def rip(job, logfile):
     # RIP WITH MAKEMKV
     # create raw directory for rip output
     # set staging directory path to raw directory output
-    # ALWAYS RIP WITH MAKEMKV, MORE WORK BUT LESS HASSLE WHEN HANDLING
+    # construct command to run makemkv with
+    # invoke makemkv_dvd with constructed command (ALWAYS RIP WITH MAKEMKV, MORE WORK BUT LESS HASSLE WHEN HANDLING)
 
     # TRANSCODE WITH HANDBRAKE
     # if not SKIP_TRANSCODING
     #     create transcode output directory
-    #     transcode files in staging directory into transcode output directory
+    #     if TRANSCODE_MAINFEATURE_ONLY
+    #         construct command to run makemkv with
+    #         invoke handbrake_dvd with constructed command
+    #         transcode main in staging directory into transcode output directory
+    #     else
+    #         construct command to run makemkv with
+    #         invoke handbrake_dvd with constructed command
+    #         transcode files in staging directory into transcode output directory
     #     set staging directory path to transcode output directory path
 
     # SAVE PROCESSED DATA
     # Create final output directory, using the current job title (NO NEED TO CORRECT SINCE THIS IS WHEN WE CREATE IT)
-    # copy files from staging directory to final output directory
+    # copy files from staging directory to final output directory (ACCOUNT FOR TRANSCODE_MAINFEATURE_ONLY)
     # Save poster image from disc if enabled - MOVE THIS TO MAIN
 
     # CLEAN UP FILES
