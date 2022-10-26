1cf398d089b6ddef6003b3724a90493ca2e87dd8
wolfy <wolfy@shitwolfymakes.com>
2022-10-25 19:59:47 -0600

    added implementation details

--- arm/ripper/ripper_dvd.py
+++ arm/ripper/ripper_dvd.py
@@ -1,14 +1,33 @@
+import arm.config.config as cfg
+from arm.models.models import Job
+from arm.ripper import utils
 
-def rip(job, logfile):
+
+def rip(job):
+    """
+    Top-level ripping function for dvds, be they movies or series
+    :param job: The current job that is being worked on
+    """
+    # have_dupes, logfile, and protection can all be derived from
+    # the data stored in a Job
+
     # PREP JOB DATA
     # Set the sub-folder type - (movie|tv|unknown)
+    type_sub_folder = utils.convert_job_type(job.video_type)
     # Fix the job title - Title (Year) | Title
+    job_title = utils.fix_job_title(job)
 
-    # CHECK FOR INVALID CONDITIONS
+    # CHECK FOR EXCEPTION CONDITIONS
     # check if duplicates are allowed
-    #     return with non-zero code if not to enable testing
+    have_dupes = utils.job_dupe_check(job)
+    if cfg.arm_config["ALLOW_DUPLICATES"] is False:
+        # return with non-zero code if not to enable testing
+        pass
+
     # check if 99 track rips are allowed - IF THIS DISK HAS 99 TRACK PROTECTION, THAT SHOULD ALREADY BE STORED IN JOB
-    #     return with non-zero code if not to enable testing
+    if cfg.arm_config["PREVENT_99"] is True:
+        # return with non-zero code if not to enable testing
+        pass
 
     # RIP WITH MAKEMKV
     # create raw directory for rip output
